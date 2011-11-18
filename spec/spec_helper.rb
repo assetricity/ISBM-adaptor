1d0c9b08c0855f547cccea83243c717649041ba6
BrandonMathis <Brandon@KeysetTS.com>
2011-11-18 14:13:02 -0500

    Removed isbm.yml endpoints that people dont need

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,7 @@ require 'log_buddy'
 $:.unshift File.expand_path('..', __FILE__)
 
 ENV["RACK_ENV"] = 'test'
-Isbm::Config.load!(File.join("spec", "fixtures", "isbm.yml"))
+Isbm::Config.load!(File.join("config", "isbm.yml"))
 
 RSpec.configure do |config|
-  # config.before(:all) do
-  # end
-
-  # config.after(:all) do
-  #   channels_after_specs = Isbm::ChannelManagement.get_all_channels
-  #   puts "ISBM was left with #{ channels_after_specs.length - @channels.length} open test channels" if @channels.length != channels_after_specs.length
-  # end
 end
