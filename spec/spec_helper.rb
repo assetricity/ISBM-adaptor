57cff1703b87fc084627e8762c939943b3a7a8e6
Avin Mathew <amathew@assetricity.com>
2012-07-31 11:26:14 +1000

    Remove unneeded gem reference

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,8 @@ require 'rubygems'
 require 'rspec'
 require 'rspec/given'
 require 'isbm'
-require 'log_buddy'
 
 $:.unshift File.expand_path('..', __FILE__)
 
 ENV["RACK_ENV"] = 'test'
 Isbm::Config.load!(File.join("config", "isbm.yml"))
-
-RSpec.configure do |config|
-end
