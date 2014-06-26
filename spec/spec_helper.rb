6c275ce4e07c757928ee239e096e5ee4001725a6
Avin Mathew <amathew@assetricity.com>
2014-06-27 09:10:33 +1000

    Rerecord cassettes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require 'rubygems'
-require 'active_support/core_ext/hash'
 require 'active_support/inflector'
 require 'rspec'
 require 'webmock/rspec'
@@ -16,19 +15,11 @@ ENDPOINTS = settings['endpoints']
 OPTIONS = settings['options']
 
 VCR.configure do |c|
-  c.default_cassette_options = { record: :new_episodes }
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
-  c.ignore_localhost = true
+  c.configure_rspec_metadata!
 end
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
-
-  # Make a test use VCR by tagging it with :vcr
-  config.around(:each, :vcr) do |example|
-    name = example.metadata[:full_description].split(/\s+/, 2).join('/').underscore.gsub(/[^\w\/]+/, '_')
-    options = example.metadata.slice(:record, :match_requests_on).except(:example_group)
-    VCR.use_cassette(name, options) { example.call }
-  end
 end
